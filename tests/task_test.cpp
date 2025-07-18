e492bf6cda010dbe353dc4ed67d59daa0dbc044e
Ruben Alvarez-Gutierrez <ruben513@ucsb.edu>
2025-07-17 19:12:39 -0700

    cleaned up some naming and comments

--- tests/task_test.cpp
+++ tests/task_test.cpp
@@ -7,9 +7,7 @@
 // Toggles the suppressOutput flag to true (see output_control.h if interested)
 static SuppressOutputSetter _suppressOutputtSetter; 
 
-void dummyFunction(void* args) {
-    return;
-}
+void dummyFunction(void* args) {return;}
 
 TEST_CASE("Tasks initialize correctly") {
     uint8_t dummyPriority = 4;
