5986be31842e8d55bd7700120387b5aae7140e2b
Ruben Alvarez-Gutierrez <ruben513@ucsb.edu>
2025-07-17 19:11:08 -0700

    added tests for dynamic task creation

--- tests/rtos_test.cpp
+++ tests/rtos_test.cpp
@@ -5,12 +5,25 @@
 #include "config.h"
 #include "output_control.h"
 
-static SuppressOutputSetter _suppressOutputtSetter; // Toggles the suppressIdleOutput flag to true
+// Toggles the suppressOutput flag to true (see output_control.h if interested)
+static SuppressOutputSetter _suppressOutputtSetter; 
 
+// Used to log the execution of tasks.
 static std::vector<std::string> log;
 
-void taskFunction(void* args) {
+// Simulated task functions for testing.
+// These are passed into createTask to verify task scheduling behavior.
+
+void logTask(void* args) {
   auto ctx = static_cast<TaskContext*>(args);
+  log.push_back(ctx->task->getName());
+}
+
+void logTaskWithDynamic(void* args) {
+  auto ctx = static_cast<TaskContext*>(args);
+  if (ctx->task->getName() == "Creator") {
+    ctx->rtos->createTask("Dynamically Created Task", 2, logTaskWithDynamic);
+  }
   log.push_back(ctx->task->getName());
 }
 
@@ -19,7 +32,7 @@ TEST_CASE("Function executes correct number of times") {
 
   std::size_t memSize = 1000;
   uint8_t priority = 1;
-  taskFunction_t taskCode = taskFunction;
+  taskFunction_t taskCode = logTask;
 
   RTOS rtos(memSize);
   rtos.createTask("task", priority, taskCode);
@@ -35,7 +48,7 @@ TEST_CASE("Function executes higher priority task first") {
 
   std::size_t memSize = 1000;
   uint8_t priority1 = 5, priority2 = 1;
-  taskFunction_t taskCode = taskFunction;
+  taskFunction_t taskCode = logTask;
 
   RTOS rtos(memSize);
   rtos.createTask("HigherPriority", priority1, taskCode);
@@ -51,7 +64,7 @@ TEST_CASE("Round-robin scheduling amongst equal priority tasks") {
 
   std::size_t memSize = 5000;
   uint8_t priority = 1;
-  taskFunction_t taskCode = taskFunction;
+  taskFunction_t taskCode = logTask;
 
   RTOS rtos(memSize);
   rtos.createTask("task1", priority, taskCode);
@@ -74,13 +87,43 @@ TEST_CASE("Round-robin scheduling amongst equal priority tasks") {
   }
 }
 
+TEST_CASE("Tasks are able to be created dynamically") {
+  log.clear();
+
+  std::size_t memSize = 1000;
+  uint8_t priority = 5;
+  taskFunction_t taskCode = logTaskWithDynamic;
+
+  RTOS rtos(memSize);
+  rtos.createTask("Creator", priority, taskCode);
+  rtos.startScheduler();
+
+  REQUIRE(log[configNUM_TASK_STEPS] == "Dynamically Created Task");
+}
+
+TEST_CASE("Dynamically created task begins execution on the next clock tick if higher priority") {
+  log.clear();
+
+  std::size_t memSize = 1000;
+  uint8_t priority = 1;
+  taskFunction_t taskCode = logTaskWithDynamic;
+
+  RTOS rtos(memSize);
+  rtos.createTask("Creator", priority, taskCode);
+  rtos.startScheduler();
+
+  REQUIRE(log[0] == "Creator");
+  REQUIRE(log[1] == "Dynamically Created Task");
+  REQUIRE(log[1+configNUM_TASK_STEPS] == "Creator"); // original task resumes execution
+}
+
 TEST_CASE("Task creation fails when memory is insufficient") {
   log.clear();
 
   std::size_t memSize = configSTACK_SIZE - 1;
 
   RTOS rtos(memSize);
-  rtos.createTask("ShouldFail", 1, taskFunction);
+  rtos.createTask("ShouldFail", 1, logTask);
   rtos.startScheduler();
 
   REQUIRE(log.empty());
@@ -92,8 +135,8 @@ TEST_CASE("Task creation fails with invalid priority") {
   std::size_t memSize = 1000;
 
   RTOS rtos(memSize);
-  rtos.createTask("TooHigh", configMAX_PRIORITY + 1, taskFunction);
-  rtos.createTask("TooLow", -1, taskFunction);
+  rtos.createTask("TooHigh", configMAX_PRIORITY + 1, logTask);
+  rtos.createTask("TooLow", -1, logTask);
   rtos.startScheduler();
 
   REQUIRE(log.empty());
@@ -114,11 +157,11 @@ TEST_CASE("Creating maximum number of tasks that barely fit in memory") {
   std::size_t memSize = configSTACK_SIZE * 3;
 
   RTOS rtos(memSize);
-  rtos.createTask("T1", 1, taskFunction);
-  rtos.createTask("T2", 1, taskFunction);
-  rtos.createTask("T3", 1, taskFunction);
+  rtos.createTask("T1", 1, logTask);
+  rtos.createTask("T2", 1, logTask);
+  rtos.createTask("T3", 1, logTask);
 
-  rtos.createTask("T4", 1, taskFunction); // this one should fail
+  rtos.createTask("T4", 1, logTask); // this one should fail
   rtos.startScheduler();
 
   REQUIRE(log.size() == 3 * configNUM_TASK_STEPS);
